ba58cfafb4d498c59b768300da6f2dcca84ae21c
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-07-18 11:23:44 -0700

    adding apply button and styling

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import JoblyApi from "../helper/api";
+import job from "../images/jobs.svg";
 import "../styles/jobs.css";
 
 const Jobs = () => {
   const [jobs, setJobs] = useState([]);
+
+  // replace 'testUser' with the actual username
+  const username = "testUser";
 
   useEffect(() => {
     async function getJobsFromAPI() {
@@ -14,21 +18,33 @@ const Jobs = () => {
     getJobsFromAPI();
   }, []);
 
+  const handleApply = async (jobId) => {
+    await JoblyApi.applyToJob(username, jobId);
+    setJobs(
+      jobs.map((job) => (job.id === jobId ? { ...job, applied: true } : job))
+    );
+  };
+
   return (
-    <div>
-      <h1>Job Listings</h1>
+    <div className="jobs">
+      <div className="jobs-header">
+        <h1>Job Listings</h1>
+        <img src={job} alt="jobs" />
+      </div>
       <div className="jobs-container">
         {jobs.map((job, index) => (
           <div className="job-card" key={index}>
             <h2>{job.title}</h2>
             <p>{job.companyHandle}</p>
             <p>
-              <strong>Salary:</strong>{" "}
-              {job.salary !== null ? job.salary : "not specified"}
+              <strong>Salary:</strong> {job.salary}
             </p>
             <p>
               <strong>Equity:</strong> {job.equity !== null ? job.equity : 0}
             </p>
+            <button onClick={() => handleApply(job.id)}>
+              {job.applied ? "Applied" : "Apply"}
+            </button>
           </div>
         ))}
       </div>

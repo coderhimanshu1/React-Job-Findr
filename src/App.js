5cea7a7df862f59b02a2f6e53ab1a32781f47f29
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-07-18 00:47:17 -0700

    adding register page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from "./logo.svg";
 import Home from "./routes/home";
 import Login from "./routes/login";
+import Register from "./routes/register";
 import NavBar from "./routes/nav";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
@@ -15,6 +16,9 @@ function App() {
         </Routes>
         <Routes>
           <Route path="/login" element={<Login />} />
+        </Routes>
+        <Routes>
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
     </div>
